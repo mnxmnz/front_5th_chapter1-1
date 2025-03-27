28aceb74218f9c7ed536891af42d4d30eee46b58
mnxmnz <minzidev@gmail.com>
2025-03-28 03:34:06 +0900

    feat: 사용자 로그인 및 로그아웃 기능 추가와 로컬 스토리지 관리 개선

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,52 @@
-import { getLocalStorageItem } from "./storage.js";
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+  removeLocalStorageItem,
+} from "./storage.js";
 
-const auth = {
-  user: getLocalStorageItem("user"),
-  isLoggedIn: getLocalStorageItem("user") ? true : false,
+import auth from "../store/auth.js";
+
+const LOCAL_STORAGE_KEY = "user";
+
+const signIn = ({ username, email, bio }) => {
+  const userInfo = { username, email, bio };
+
+  auth.setUserInfo(userInfo);
+  setLocalStorageItem(LOCAL_STORAGE_KEY, userInfo);
+
+  window.navigateTo("/");
+
+  return userInfo;
 };
 
-export { auth };
+const signOut = () => {
+  const currentUser = getLocalStorageItem(LOCAL_STORAGE_KEY);
+
+  if (!currentUser) {
+    return;
+  }
+
+  auth.clearUserInfo();
+  removeLocalStorageItem(LOCAL_STORAGE_KEY);
+
+  window.navigateTo("/login");
+};
+
+const getIsLoggedIn = () => {
+  return auth.getIsLoggedIn();
+};
+
+const restoreAuth = () => {
+  const userInfo = getLocalStorageItem(LOCAL_STORAGE_KEY);
+
+  if (!userInfo) {
+    auth.clearUserInfo();
+    return;
+  }
+
+  auth.setUserInfo(userInfo);
+
+  return userInfo;
+};
+
+export { signIn, signOut, getIsLoggedIn, restoreAuth };
