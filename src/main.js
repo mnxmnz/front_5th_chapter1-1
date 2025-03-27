4097d3df752638f9b113785ef53c87f2026d0dae
mnxmnz <minzidev@gmail.com>
2025-03-28 03:34:26 +0900

    feat: 라우터 통합 및 페이지 구조 변경, 이벤트 리스너 설정 개선

--- src/main.js
+++ src/main.js
@@ -1,79 +1,20 @@
 import Layout from "./components/Layout.js";
 
-import HomePage from "./components/HomePage.js";
-import ProfilePage from "./components/ProfilePage.js";
-import LoginPage from "./components/LoginPage.js";
-import NotFoundPage from "./components/NotFoundPage.js";
+import MainPage from "./pages/main.js";
+import ProfilePage from "./pages/profile.js";
+import LoginPage from "./pages/login.js";
+import ErrorPage from "./pages/error.js";
 
-import { redirectToPath } from "./utils/route.js";
-import {
-  setLocalStorageItem,
-  removeLocalStorageItem,
-} from "./utils/storage.js";
+import Router from "./utils/route.js";
+import { setupEventListeners } from "./utils/eventHandlers.js";
 
 const routes = {
-  "/": Layout(HomePage()),
-  "/login": LoginPage(),
-  "/profile": Layout(ProfilePage()),
+  "/": () => Layout(MainPage()),
+  "/login": () => LoginPage(),
+  "/profile": () => Layout(ProfilePage()),
+  "/error": () => ErrorPage(),
 };
 
-function renderRoute() {
-  const currentRoute = window.location.pathname;
+const router = new Router(routes);
 
-  if (routes[currentRoute]) {
-    document.body.innerHTML = routes[currentRoute];
-    window.history.pushState({ path: currentRoute }, "", currentRoute);
-    return;
-  }
-
-  document.body.innerHTML = NotFoundPage();
-  window.history.pushState({ path: currentRoute }, "", currentRoute);
-}
-
-function handleLogin() {
-  const username = document.getElementById("username").value;
-
-  setLocalStorageItem("user", {
-    username,
-    email: "",
-    bio: "",
-  });
-  redirectToPath("/");
-}
-
-function handleLogout() {
-  removeLocalStorageItem("user");
-  redirectToPath("/");
-}
-
-function handleProfile() {
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const bio = document.getElementById("bio").value;
-
-  setLocalStorageItem("user", { username, email, bio });
-  redirectToPath("/");
-}
-
-function setupEventListeners() {
-  window.addEventListener("popstate", renderRoute);
-  window.addEventListener("load", renderRoute);
-  window.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    if (event.target.id === "login-form") {
-      handleLogin();
-    }
-
-    if (event.target.id === "profile-form") {
-      handleProfile();
-    }
-  });
-  window.addEventListener("click", (event) => {
-    if (event.target.id === "logout") {
-      handleLogout();
-    }
-  });
-}
-
-setupEventListeners();
+setupEventListeners(router);
